da9268beec49ad25b69a682b0fab31052b3d6c0b
Tiago Fernandes <tiago@zavamed.com>
2018-05-06 21:46:44 +0100

    change koa for express and express static

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-require("babel/register");
-require("./server/index");
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+app.use(serveStatic(path.join(__dirname, 'content')));
+
+app.listen(process.env.PORT || 3000);
